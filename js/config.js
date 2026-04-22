d206bd0c291712ec73b64b3ceb488eab000e89fe
lusi-studio <lu.soporte.studio@gmail.com>
2026-04-21 21:49:15 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
+export const ADMIN_EMAIL = "johangsuite2@gmail.com";
 export const config = {
   supabaseUrl: "https://zplpykgkxgztocftbxmb.supabase.co",
   supabasePublishableKey: "sb_publishable_Ls4PMfvWtJcM9u9i6khEdg_il_f9-JX",
